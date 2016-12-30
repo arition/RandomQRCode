e0755a7d20b388bb2d67683e54af98e9aec28c59
arition <aritionkb@gmail.com>
2016-12-30 00:13:18 -0800

    Finish

--- RandomQRCode/MainWindow.xaml.cs
+++ RandomQRCode/MainWindow.xaml.cs
@@ -1,28 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using QRCoder;
+using System.Drawing;
+using System.IO;
 
 namespace RandomQRCode
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        public Random Random { get; } = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await Dispatcher.InvokeAsync(
+                        () => QRImage.Source = BitmapToImageSource(GenerateQRCode(Random.Next().ToString())));
+                    await Task.Delay(10000);
+                }
+            });
+        }
+
+        public Bitmap GenerateQRCode(string data)
+        {
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCode(qrCodeData);
+            return qrCode.GetGraphic(20);
+        }
+
+        public BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                var bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        private void QRImage_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            QRImage.Source = BitmapToImageSource(GenerateQRCode(Random.Next().ToString()));
         }
     }
 }
